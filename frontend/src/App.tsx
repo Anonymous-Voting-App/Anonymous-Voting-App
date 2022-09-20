8377099652f17d6285d552393393e83155f61bd1
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-09-20 20:34:58 +0300

    Add simple test status check

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // Just testing, this can be removed :)
+    const backendAddress =
+        (process.env.REACT_APP_BACKEND_HOST || window.location.origin) +
+        '/api/';
+    console.log('Backend:' + backendAddress);
+    console.log('Env:' + process.env.NODE_ENV);
+
+    const testBackend = async () => {
+        try {
+            const res = await fetch(backendAddress + 'health');
+            const json = await res.json();
+
+            setBackendStatus(json.server);
+            setDatabaseStatus(json.database);
+        } catch {
+            setBackendStatus(false);
+            setDatabaseStatus(false);
+        }
+    };
+
+    const [backendStatus, setBackendStatus] = useState(false);
+    const [databaseStatus, setDatabaseStatus] = useState(false);
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    Backend {backendStatus ? 'connected' : 'not connected'}
+                    <br />
+                    Database {databaseStatus ? 'connected' : 'not connected'}
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <button onClick={testBackend}>Check connection</button>
             </header>
         </div>
     );
