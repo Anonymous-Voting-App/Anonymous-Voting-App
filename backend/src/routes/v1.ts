31347f11a5ff15c23fa192b5b496f91570233768
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-09-21 11:57:29 +0300

    Init Swagger/OpenAPI

--- backend/src/routes/v1.ts
+++ backend/src/routes/v1.ts
@@ -1,7 +1,25 @@
 import { Router } from 'express';
 import { router as healthRouter } from './v1/health';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Anonymous Voting App',
+            version: '1.0.0'
+        }
+    },
+    apis: ['**/v1/*.ts']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const router = Router();
+
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerSpec));
 
 router.use('/health', healthRouter);
 
--- backend/src/routes/v1/health.ts
+++ backend/src/routes/v1/health.ts
@@ -4,6 +4,26 @@ export const router = Router();
 
 /**
  * Health check
+ * @openapi
+ * /api/health:
+ *  get:
+ *    description: Health check for backend and database connection
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      200:
+ *        description: Status of server and database
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                server:
+ *                  type: boolean
+ *                  example: true
+ *                database:
+ *                  type: boolean
+ *                  example: false
  */
 router.get('/', (req: Request, res: Response) => {
     // TODO: When database implemented, add health check here!
