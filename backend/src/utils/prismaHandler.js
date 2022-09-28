4f20790364200b452fd13c794581a4ed9b679e43
Joonas Halinen <joonas.halinen@tuni.fi>
2022-09-28 11:53:10 +0300

    Delete .js files generated by ts compiler

--- backend/src/utils/prismaHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = require("@prisma/client");
-// Re use single Prisma instance
-// https://www.prisma.io/docs/guides/performance-and-optimization/connection-management
-var prisma = new client_1.PrismaClient();
-exports.default = prisma;
