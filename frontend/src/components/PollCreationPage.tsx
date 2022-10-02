7613bbfdcdd214adc961f132f2f61eb8ded1ea23
Vrinda VJ <vrinda@Vrindas-MacBook-Pro.local>
2022-10-02 22:29:01 +0300

    poll creation - implemented addition of question and options

---
+++ frontend/src/components/PollCreationPage.tsx
@@ -0,0 +1,120 @@
+import { Container, Typography, TextField, Button } from '@mui/material'
+import React, { useState, useEffect } from 'react';
+import { ArrowRightAlt, AddCircleOutline } from '@mui/icons-material';
+import './PollCreationPage.scss';
+import QuestionComponent from './QuestionComponent';
+
+
+function PollCreationPage() {
+    const [showQuesContainer, setShowQuesContainer] = useState(false)
+    const [questions, setQuestions] = useState([{ text: '', type: '', options: [''] }])
+    const [pollName, setPollName] = useState('');
+
+
+    /**
+     * Function to add empty object to array when add question btn is clicked
+     */
+    const addQuestion = () => {
+        setShowQuesContainer(true)
+
+        //    condition check to handle empty question type,
+        //    if empty dont increment array 
+        const isEmpty = questions.findIndex(question => question.text === ''
+            || question.type === ''
+            || (question.options.findIndex(option => option === '') === -1 ? false : true)) === -1 ? false : true;
+
+        if (!isEmpty) {
+            setQuestions([
+                ...questions, { text: '', type: '', options: [''] }
+            ])
+        }
+    }
+
+    /**
+     * Function to update question array with entered question text
+     * @param value - question text
+     * @param index - index of question in the array
+     */
+    const onQuestionInput = (value: string, index: number) => {
+        const newQuestions = questions.map((question, questionIndex) => {
+            if (questionIndex === index) {
+                return { text: value, type: question.type, options: question.options }
+            }
+            return question
+        })
+        setQuestions(newQuestions)
+    }
+
+    /**
+     * Function to update question array with question options
+     * @param newOptions - array of options for the question
+     * @param index - index of question in the array
+     */
+    const onOptionInput = (newOptions: [string], index: number) => {
+        const newQuestions = questions.map((question, questionIndex) => {
+            if (questionIndex === index) {
+                return { text: question.text, type: question.type, options: newOptions }
+            }
+            return question
+        })
+        setQuestions(newQuestions)
+    }
+
+    /**
+     * Function to update question array with question type
+     * @param value - question type
+     * @param index - index of question in the array
+     */
+    const onTypeChange = (value: string, index: number,) => {
+        const newQuestions = questions.map((question, questionIndex) => {
+            if (questionIndex === index) {
+                return { text: question.text, type: value, options: question.options }
+            }
+            return question
+        })
+        setQuestions(newQuestions)
+    }
+
+/**
+ * Function to handle the submit event
+ */
+    const submitHandler = () => {
+        console.log(questions, pollName)
+    }
+
+    return (
+        <Container className='main-wrapper'>
+            <div className='page-heading'>
+                <Typography variant='h4'> Create poll </Typography>
+                <ArrowRightAlt />
+            </div>
+            <div className='poll-name'>
+                <TextField
+                    autoComplete='off'
+                    id="pollName"
+                    label="Poll name"
+                    variant="standard"
+                    onChange={(event) => setPollName(event.target.value)} />
+            </div>
+            
+            {showQuesContainer ?
+                <div className='question-container' >
+                    {questions.map((question, index) => (
+                        <div key={index} >
+                            <QuestionComponent ques={question}
+                                ind={index}
+                                typeChangehandler={onTypeChange}
+                                questionInputHandler={onQuestionInput}
+                                optionInputHandler={onOptionInput} ></QuestionComponent>
+                        </div>
+                    ))}
+                </div> : null}
+
+            <Button className='add-ques-btn' onClick={addQuestion} sx={{ mt: "4.5rem", width: 200 }} variant="outlined"><AddCircleOutline />Add a question</Button>
+            <Button variant='contained' onClick={submitHandler} className='submit-poll-btn' sx={{ mt: "4.5rem", width: 200 }}  > Submit Poll </Button>
+
+        </Container>
+    )
+}
+
+export default PollCreationPage
---
+++ frontend/src/components/QuestionComponent.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import { KeyboardArrowDown, AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material';
+import InputAdornment from '@mui/material/InputAdornment';
+
+import './QuestionComponent.scss';
+
+function QuestionComponent(props: any) {
+    const [showOptionBtn, setShowOptionBtn] = useState(false)
+    const [quesOptions, setQuesOptions] = useState(['', ''])
+    const [inputWidth, setInputWidth] = useState('260px')
+
+    /**
+     * Function to pass entered question text to pollcreation page(parent)
+     * @param value 
+     */
+    const onQuestionInput = (value: string) => {
+        setInputWidth(((value.length + 1) * 8) + 'px'); //dynamically setting the width of question input field
+        props.questionInputHandler(value, props.ind)
+    }
+
+    /**
+     * Function to pass question type to pollcreation page
+     * @param value 
+     */
+    const onTypeChange = (value: string) => {
+        props.typeChangehandler(value, props.ind);
+        setShowOptionBtn(true);
+    }
+
+    /**
+     * Function to add empty element to the quesOptions 
+     * array on add option btn click
+     */
+    const addOption = () => {
+        setQuesOptions([
+            ...quesOptions, ''
+        ])
+    }
+
+    /**
+     * Function to remove option from quesOptions array 
+     * @param index - index number of option to be removed
+     */
+    const removeOption = (index: number) => {
+        const newOptions = quesOptions.filter((option, i) => { return i !== index });
+        setQuesOptions(newOptions);
+        props.optionInputHandler(newOptions, props.ind)
+
+    }
+
+/**
+ * Function to update the options array when option text is entered 
+ * @param index - index of question for which options are added
+ * @param value - option text
+ */
+    const onOptionInput = (index: number, value: string) => {
+        const newOptions = quesOptions.map((quesOption, optionIndex) => {
+            if (optionIndex === index) {
+                return value
+            }
+            return quesOption
+        })
+        setQuesOptions(newOptions)
+        props.optionInputHandler(newOptions, props.ind)
+    }
+
+    return (
+        <>
+            <div className='question-wrapper' >
+                <FormControl className='type-dropdown' sx={{ m: 1, minWidth: 120 }} size="small">
+                    <InputLabel id="demo-select-small">Question type</InputLabel>
+                    <Select
+                        IconComponent={KeyboardArrowDown}
+                        labelId="demo-select-small"
+                        id="demo-select-small"
+                        value={props.ques.type}
+                        label="Question Type"
+                        onChange={(event) => onTypeChange(event.target.value)}
+                    >
+                        <MenuItem value={""}>
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={'radioBtn'}>Pick One</MenuItem>
+                        <MenuItem value={'checkBox'}>Multiple choice</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField sx={{ minWidth: inputWidth }}
+                    autoComplete='off'
+                    className='question-field'
+                    onChange={(event) => onQuestionInput(event.target.value)}
+                    type="text"
+                    value={props.ques.text}
+                    variant="outlined" />
+                {showOptionBtn ?
+                    <>
+                        {quesOptions.map((quesOption, index) => (
+                            <TextField
+                                className='option-field'
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <RemoveCircleOutline onClick={() => removeOption(index)} />
+                                        </InputAdornment>
+                                    )
+                                }}
+                                autoComplete='off'
+                                variant="outlined"
+                                onChange={(event) => onOptionInput(index, event.target.value)}
+                                key={index} type="text" value={quesOption}
+                            />
+                        ))}
+                        <Button className='add-option-btn' onClick={addOption} sx={{ mt: "4.5rem", width: 200 }} variant="outlined"><AddCircleOutline />Add a new option</Button>
+                    </>
+                    : null}
+            </div>
+        </>
+
+    )
+}
+
+export default QuestionComponent
