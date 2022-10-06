36502a2bcc49c26a74a132a891dc3986f5608d10
Oona Saksman <oona.saksman@tuni.fi>
2022-10-06 15:26:02 +0300

    Move all styling to scss file

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,14 +3,11 @@ import AppBar from '@mui/material/AppBar';
 import PersonIcon from '@mui/icons-material/Person';
 import KnowItLogo from './knowit.svg';
 import AVALogo from './ava.svg';
+import './NavBar.scss';
 
 const NavBar = () => {
     return (
-        <AppBar
-            className="navigation"
-            elevation={0}
-            style={{ backgroundColor: '#333333', position: 'relative' }}
-        >
+        <AppBar className="navigation">
             <img className="know-it" src={KnowItLogo} alt="Knowit Logo" />
 
             <img
@@ -21,10 +18,7 @@ const NavBar = () => {
 
             <Typography className="login">Login</Typography>
 
-            <PersonIcon
-                className="profile"
-                style={{ fontSize: 40, color: '#333333' }}
-            ></PersonIcon>
+            <PersonIcon className="profile"></PersonIcon>
         </AppBar>
     );
 };
