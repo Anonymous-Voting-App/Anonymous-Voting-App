1862b8c0e7711c295e49d251d6420e904e2232f4
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-10-11 21:08:02 +0300

    Move swagger documentation to separate file for better maintainability

--- backend/src/routes/v1/health.ts
+++ backend/src/routes/v1/health.ts
@@ -5,25 +5,5 @@ export const router = Router();
 
 /**
  * Health check
- * @openapi
- * /api/health:
- *  get:
- *    description: Health check for backend and database connection
- *    produces:
- *      - application/json
- *    responses:
- *      200:
- *        description: Status of server and database
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                server:
- *                  type: boolean
- *                  example: true
- *                database:
- *                  type: boolean
- *                  example: true
  */
 router.get('/', checkHealth);
