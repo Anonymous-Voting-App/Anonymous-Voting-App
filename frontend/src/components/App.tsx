399ce56f928bdb291ebad779ed6b50ac492cb955
Oona Saksman <oona.saksman@tuni.fi>
2022-10-19 12:33:16 +0300

    Create register page

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import NavBar from './NavBar';
 import PollCreationPage from './PollCreationPage';
+//import RegisterPage from './RegisterPage'
 //import LoginPage from './LoginPage';
 //import PollAnsweringPage from './PollAnsweringPage';
 
@@ -8,9 +9,10 @@ function App() {
     return (
         <div className="App">
             <NavBar></NavBar>
-            {/*<LoginPage></LoginPage>*/}
             {/*For now since we don't have routing, comment out the components
             you don't want to see.*/}
+            {/*<RegisterPage></RegisterPage>*/}
+            {/*<LoginPage></LoginPage>*/}
             <PollCreationPage></PollCreationPage>
             {/*<PollAnsweringPage></PollAnsweringPage>*/}
         </div>
--- frontend/src/components/RegisterPage.tsx
+++ frontend/src/components/RegisterPage.tsx
@@ -3,24 +3,27 @@ import {
     Typography,
     Container,
     Button,
-    Link,
     FormControl,
     InputLabel,
     OutlinedInput
 } from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockIcon from '@mui/icons-material/Lock';
-import './LoginPage.scss';
+import './RegisterPage.scss';
 
 function LoginPage(props: any) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
     const [shrinkName, setShrinkName] = useState(false);
     const [notchedName, setNotchedName] = useState(false);
     const [moveName, setMoveName] = useState('label move-right');
     const [shrinkPassword, setShrinkPassword] = useState(false);
     const [notchedPassword, setNotchedPassword] = useState(false);
     const [movePassword, setMovePassword] = useState('label move-right');
+    const [shrinkPassword2, setShrinkPassword2] = useState(false);
+    const [notchedPassword2, setNotchedPassword2] = useState(false);
+    const [movePassword2, setMovePassword2] = useState('label move-right');
 
     const onNameInput = (value: string) => {
         setUsername(value);
@@ -28,6 +31,10 @@ function LoginPage(props: any) {
 
     const onPasswordInput = (value: string) => {
         setPassword(value);
+    };
+
+    const onPassword2Input = (value: string) => {
+        setPassword2(value);
     };
 
     const handleFocusName = () => {
@@ -58,10 +65,24 @@ function LoginPage(props: any) {
         }
     };
 
+    const handleFocusPassword2 = () => {
+        setShrinkPassword2(true);
+        setNotchedPassword2(true);
+        setMovePassword2('label');
+    };
+
+    const handleBlurPassword2 = (value: string) => {
+        if (!value) {
+            setShrinkPassword2(false);
+            setNotchedPassword2(false);
+            setMovePassword2('label move-right');
+        }
+    };
+
     return (
         <Container className="main-wrapper">
             <Typography variant="h4" className="page-heading">
-                Login
+                Register
             </Typography>
             <Container className="fields">
                 <FormControl
@@ -129,20 +150,47 @@ function LoginPage(props: any) {
                     />
                 </FormControl>
 
+                <FormControl
+                    sx={{ minWidth: '260px' }}
+                    size="small"
+                    className="user-field"
+                >
+                    <InputLabel
+                        id="password-label"
+                        htmlFor="Password again"
+                        className={movePassword2}
+                        shrink={shrinkPassword2}
+                    >
+                        Password again
+                    </InputLabel>
+                    <OutlinedInput
+                        type="text"
+                        onFocus={handleFocusPassword2}
+                        onBlur={(event) =>
+                            handleBlurPassword2(event.target.value)
+                        }
+                        onChange={(event) =>
+                            onPassword2Input(event.target.value)
+                        }
+                        value={'*'.repeat(password2.length)}
+                        className="input-field"
+                        label="Password again"
+                        id="Password again"
+                        notched={notchedPassword2}
+                        autoComplete="off"
+                        startAdornment={
+                            <LockIcon className="input-field-icon" />
+                        }
+                    />
+                </FormControl>
+
                 <Button
                     className="login-btn"
                     sx={{ mt: '4.5rem', width: 200 }}
                     variant="outlined"
                 >
-                    Login
+                    Register
                 </Button>
-            </Container>
-
-            <Container className="texts">
-                <Typography className="bottom-text">
-                    Don't have an account?
-                </Typography>
-                <Link className="bottom-text link">Register</Link>
             </Container>
         </Container>
     );
