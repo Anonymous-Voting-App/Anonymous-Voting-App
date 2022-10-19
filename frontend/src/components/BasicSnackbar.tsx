96438e8f1ba266544ccf4fe30aca258db8ad1c87
Vrinda VJ <vrinda@Vrindas-MacBook-Pro.local>
2022-10-19 11:04:04 +0300

    snackbar time fixed - latest dev changes pulled

--- frontend/src/components/BasicSnackbar.tsx
+++ frontend/src/components/BasicSnackbar.tsx
@@ -15,7 +15,7 @@ function BasicSnackbar(props: any) {
         <>
             <Snackbar
                 open={props.open}
-                autoHideDuration={666000}
+                autoHideDuration={6000}
                 onClose={props.onClose}
                 anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
             >
