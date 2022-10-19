f9871da7a71cad271b061eddc38bc95fb241bcbc
Vrinda VJ <vrinda@Vrindas-MacBook-Pro.local>
2022-10-19 16:06:16 +0300

    build warnings resolved

--- frontend/src/components/BasicSnackbar.tsx
+++ frontend/src/components/BasicSnackbar.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
 import './BasicSnackbar.scss';
--- frontend/src/components/MainView.tsx
+++ frontend/src/components/MainView.tsx
@@ -1,1 +1,5 @@
 import React from 'react';
+
+export const MainView = () => {
+    return <div>MainView</div>;
+};
