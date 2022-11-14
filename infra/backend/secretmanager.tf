1f21edd4832695a092721eb42d79a80861365d16
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-11-14 19:02:05 +0200

    Infra updates for authentication

--- infra/backend/secretmanager.tf
+++ infra/backend/secretmanager.tf
@@ -5,7 +5,24 @@ resource "aws_secretsmanager_secret" "database_secret" {
   tags                    = local.tags
 }
 
-resource "aws_secretsmanager_secret_version" "set_secret" {
+resource "aws_secretsmanager_secret" "jwt_secret" {
+  name                    = "ava-${local.environment}-jwt"
+  description             = "JWT secret for ${local.environment}"
+  recovery_window_in_days = 0
+  tags                    = local.tags
+}
+
+resource "aws_secretsmanager_secret_version" "set_db_secret" {
   secret_id     = aws_secretsmanager_secret.database_secret.arn
   secret_string = var.db_url
+}
+
+resource "random_string" "jwt_secret" {
+  length  = 128
+  special = false
+}
+
+resource "aws_secretsmanager_secret_version" "set_jwt_secret" {
+  secret_id     = aws_secretsmanager_secret.jwt_secret.arn
+  secret_string = random_string.jwt_secret.result
 }
